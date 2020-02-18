7c8b193448d160198012c92f3b52612a8f920d62
Dwi Agus <dwiagus@uny.ac.id>
2020-02-18 14:51:22 +0700

    add pint page

--- raport/urls.py
+++ raport/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib import admin
 from . import views
-from .views import KkmCreateView,KkmUpdateView,KkmMapelListView,RaportIndexView,RaportCreateView,RaportEditView,MengajarListView
+from .views import KkmCreateView,KkmUpdateView,KkmMapelListView,RaportIndexView,RaportDetailView,RaportPrintView,RaportDownloadView,MengajarListView
 
 admin.site.site_header = "SIAKAD NUHA Admin"
 admin.site.site_title = "SIAKAD NUHA Admin Portal"
@@ -10,8 +10,9 @@ admin.site.index_title = "Sistem Informasi Akademik - SMK Ma'arif Nurul Haromain
 urlpatterns = [
     path('',views.index,name="raport-index"),
     path('raport/',RaportIndexView.as_view(),name="raport"),
-    path('raport/<slug:mapel>/',RaportCreateView.as_view(),name="raport-add"),
-    path('raport/<int:pk>/update/',RaportEditView.as_view(),name="raport-edit"),
+    path('raport/<int:pk>/detail/',RaportDetailView.as_view(),name="raport-detail"),
+    path('raport/<int:pk>/print/', RaportPrintView.as_view(), name="raport-print"),
+    path('raport/<int:pk>/download/',RaportDownloadView.as_view(),name="raport-excel"),
     path('mengajar/',MengajarListView.as_view(),name="mengajar"),
     path('kkmmapel/',KkmMapelListView.as_view(),name="kkmmapel"),
     path('kkmmapel/<int:pk>/',KkmUpdateView.as_view(),name="kkm-update"),
--- raport/views.py
+++ raport/views.py
@@ -1,20 +1,19 @@
-from django.contrib import messages
+from datetime import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.http import HttpResponse, JsonResponse, request, HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, ListView, UpdateView,FormView,View
+from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
+from django.views.generic import CreateView, ListView, UpdateView, View
 from django.shortcuts import render,redirect
 from django.db.models import Q
 from django.forms.models import inlineformset_factory
+from openpyxl import Workbook
 from guru.models import Guru
 from mapel.models import KkmMapel,Kelas,Mapel,DescMapel,Mengajar
 from siswa.models import Kelas as KelasSiswa,Siswa
 from raport.models import Raport
-from tahunajaran.models import TahunAjaran
 from .kkmform import KkmFormset,KkmmapelForm
 from .raportform import RaportForm
-import json
 
 
 # Create your views here.
@@ -28,6 +27,7 @@ class RaportIndexView(LoginRequiredMixin,ListView):
         jadwal = get_mengajar(guru)
         context = {
             'jadwal': jadwal,
+            'title': 'Rapot siswa'
         }
         return context
 
@@ -80,8 +80,62 @@ class RaportCreateView(CreateView):
             return JsonResponse({'status': 'ok', 'data': formset})
 
 
-class RaportEditView(UpdateView):
+class RaportDownloadView(View):
     model = Raport
+    def get(self, request, *args, **kwargs):
+        response = HttpResponse(
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        )
+        response['Content-Disposition'] = 'attachment; filename={date}-movies.xlsx'.format(
+            date=datetime.now().strftime('%Y-%m-%d'),
+        )
+        kkm = get_kkm(self.kwargs['pk'])
+        workbook = Workbook
+        worksheet = workbook.active
+        #worksheet.title = 'Rekap Nilai'
+        columns = ['No','NIS','NAMA PESERTA DIDIK','ANGKA','PREDIKAT','DESKRIPSI','ANGKA','PREDIKAT','DESKRIPSI']
+        row_num = 1
+
+        # Assign the titles for each cell of the header
+        for col_num, column_title in enumerate(columns, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+        workbook.save(response)
+        return response
+
+class RaportDetailView(ListView):
+    model = Raport
+
+class RaportPrintView(ListView):
+    model = Raport
+    template_name = 'raport/raport_print.html'
+    def get_context_data(self, **kwargs):
+        context = super(RaportPrintView, self).get_context_data(**kwargs)
+        kkm = KkmMapel.objects.get(pk=self.kwargs['pk'])
+        guru = kkm.mapel.guru.nama
+        nama_mapel = kkm.mapel.mapel.nama_mapel
+        tahun = kkm.tahun.tahun
+        kelas = kkm.kelas.nama_kelas
+        nilai = kkm.raport_set.all()
+        rekap_list = []
+        for ni in nilai:
+            rekap = {}
+            rekap['nis'] = ni.siswa.nis
+            rekap['siswa'] = ni.siswa.nama
+            rekap['angka_pengetahuan'] = ni.pengetahuan
+            rekap['angka_keterampilan'] = ni.keterampilan
+            rekap['predikat_pengetahuan'] = get_predikat(kkm.pengetahuan,ni.pengetahuan)
+            rekap['predikat_keterampilan'] = get_predikat(kkm.ketrampilan,ni.keterampilan)
+            rekap['desc_pengetahuan'] = kkm.descmapel_set.get(predikat=get_predikat(kkm.pengetahuan,ni.pengetahuan)).pengetahuan
+            rekap['desc_keterampilan'] = kkm.descmapel_set.get(predikat=get_predikat(kkm.ketrampilan,ni.keterampilan)).ketrampilan
+            rekap_list.append(rekap)
+        context['tahun'] = tahun
+        context['kelas'] = kelas
+        context['mypk'] = self.kwargs['pk']
+        context['guru'] = guru
+        context['nama_mapel'] = nama_mapel
+        context['nilai'] = rekap_list
+        return context
 
 class KkmUpdateView(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
     model = KkmMapel
@@ -106,11 +160,10 @@ class KkmUpdateView(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
             raport = raportformset(instance=self.object)
             for form in raport:
                 form.fields['siswa'].queryset = siswa
-
+            context['title'] =  self.object
             context['mpk'] = self.kwargs['pk']
             context['kkm_formset'] = KkmFormset(instance=self.object)
             context['raport_formset'] = raport
-
         return context
     def form_valid(self, form):
         context = self.get_context_data(form=form)
@@ -125,10 +178,7 @@ class KkmUpdateView(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
                     if raportset.is_valid():
                         raportset.instance = self.object
                         raportset.save()
-                        #messages.success(request, 'Thank you')
                         return HttpResponseRedirect('/raport/raport')
-
-                        #return response
                     else:
                         return super().form_invalid(raportset)
                 else:
@@ -155,10 +205,10 @@ class KkmCreateView(LoginRequiredMixin,SuccessMessageMixin,CreateView):
             form.fields['mapel'].queryset = Mengajar.objects.filter(guru_id=guru.id)
             form.fields['kelas'].queryset = kelas
             formset = KkmFormset()
-
             context['kkm_formset'] = formset
             context['form'] = form
             context['mpk'] = self.kwargs
+            context['title'] = "Tambah KKM"
         return context
 
     def form_valid(self, form):
@@ -170,7 +220,6 @@ class KkmCreateView(LoginRequiredMixin,SuccessMessageMixin,CreateView):
                     response = super().form_valid(form)
                     formset.instance = self.object
                     formset.save()
-
                     return response
                 else:
                     return super().form_invalid(form)
@@ -185,7 +234,7 @@ class KkmMapelListView(LoginRequiredMixin,ListView):
     model = KkmMapel
     def get_context_data(self, **kwargs):
         guru = get_guru(self.request.user.id)
-        jadwal = get_mengajar(guru)
+        jadwal = guru.mengajar_set.all()
         kkm = KkmMapel.objects.filter(mapel__guru_id=guru.id)
         context = {
             'guru': guru,
@@ -200,35 +249,12 @@ class MengajarListView(LoginRequiredMixin,ListView):
     model = Mengajar
     def get_context_data(self, **kwargs):
         guru = get_guru(self.request.user.id)
-        jadwal = get_mengajar(guru)
+        jadwal = guru.mengajar_set.all()
         context = {
             'guru': guru,
             'jadwal': jadwal,
         }
         return context
-
-def create_raport(request,mapel):
-    kkm = KkmMapel.objects.get(pk=mapel)
-    #kkm = KkmMapel.objects.get(pk=self.kwargs['mapel'])
-    siswa = KelasSiswa.objects.filter(Q(kelas_id=kkm.kelas_id) & Q(tahun_id=kkm.tahun_id))
-    # initial=[{'kelas': kkm.kelas_id},{'kelas': kkm.tahun_id},{'kelas': kkm.kelas_id},{'kelas': kkm.kelas_id}]
-    # raportformset = RaportFormSet(initial=initial)
-    # raportformset.extra_forms = len(initial)
-    if request.method == 'POST':
-        array = request.POST.getlist('pengetahuan[]')
-
-        return JsonResponse({
-            'status': 'ok',
-            'array': request.POST,
-        })
-    else:
-        context = {
-            'mapel': mapel,
-            'kkm': kkm,
-            'siswa': siswa,
-            # 'raport': raportformset
-        }
-        return render(request, 'raport/raport_add.html', context)
 
 def index(request):
     if not request.user.is_authenticated:
@@ -249,3 +275,26 @@ def get_kkm(id):
 
 def get_mengajar(guru):
     return Mengajar.objects.filter(guru_id=guru.id)
+
+def get_kkmdes(predikat):
+    pass
+
+def get_predikat(kkm, nilai):
+    nilaimax = 100
+    nilaiKkm = kkm
+    interval = (nilaimax - nilaiKkm) / 3
+    nilaiA = (nilaimax - interval)
+    nilaiB = nilaiA - interval
+    nilaiC = nilaiB - interval
+    nilaiD = nilaiC - interval
+    if nilaiA <= nilai <= nilaimax:
+        predikat = "A"
+    elif nilaiB <= nilai <= nilaiA:
+        predikat = "B"
+    elif nilaiC <= nilai <= nilaiB:
+        predikat = "C"
+    elif nilaiD <= nilai <= nilaiC:
+        predikat = "D"
+    else:
+        predikat = "E"
+    return predikat
